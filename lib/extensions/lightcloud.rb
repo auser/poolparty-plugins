7d83632da4771c43e7c33d558bff0dd8e2fc4ace
Ari Lerner <arilerner@mac.com>
2009-05-05 11:44:43 -0700

    Added lightcloud extension

---
+++ lib/extensions/lightcloud.rb
@@ -0,0 +1,50 @@
+=begin rdoc
+  Installs tokyo tyrant, tokyo cabinet and lightcloud. 
+  
+  It creates a basic config.py file that should probably be updated and made "smarter"
+  
+  Usage:
+    enable :lightcloud
+=end
+module PoolParty
+  class Base
+    plugin :lightcloud do
+      
+      def enable
+        enable :tokyo_tyrant
+        
+        install_lightcloud
+        start_lightcloud
+      end
+      
+      def install_lightcloud
+        has_exec "svn co http://opensource.plurk.com/svn/opensource/lightcloud_manager ~/lightcloud_manager && cd ~/lightcloud_manager"
+
+        has_file "~/lightcloud_manager/config.py" do
+          render :erb
+          content <<-EOC
+DATA_DIR = '~/lightcloud_manager/data'
+TOKYO_SERVER_PARMS = '#bnum=1000000#fpow=13#opts=ld'
+
+USE_MASTER = True
+
+<% %x[/usr/bin/server-list-active internal_ip].split("\t").each_with_index do |ip, index| %>
+NODES = {
+    #Lookup nodes
+    'lookup1_<%= index -%>': { 'id': <%= index %>, 'host': '127.0.0.1:41201', 'master': '127.0.0.1:51201' },
+
+    #Storage nodes
+    'storage1_<%= index -%>': { 'id': <%= index + 1 %>, 'host': '127.0.0.1:41201', 'master': '127.0.0.1:51201' },
+<% end %>
+}          
+          EOC
+end
+      end
+      
+      def start_lightcloud
+        has_exec "python -m manager all start"
+      end
+      
+    end
+  end
+end
