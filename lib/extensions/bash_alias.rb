95a101e887b66006e343afed5bf2493d75378a9e
Nate Murray <nate@natemurray.com>
2009-06-15 16:27:16 -0700

    typos

--- lib/extensions/bash_alias.rb
+++ lib/extensions/bash_alias.rb
@@ -1,12 +1,13 @@
 module PoolParty
   module Plugin
-    class BaseAlias < Plugin
-      dsl_methods :name, # the name of the cmd
-                  :value # the value of the alias
+    class BashAlias < Plugin
+      dsl_methods :name,  # the name of the cmd
+                  :value, # the value of the alias
+                  :user 
 
-      def before_load(o={}, &block)
-        user opts[:user] || "root"
-        has_line_in_file :file => "/root/.profile", :line => "alias #{name}='#{value}'"
+      def before_load(opts={}, &block)
+        # TODO, why does "has_" segfault
+        line_in_file :file => "/root/.profile", :line => "alias #{name}='#{value}'"
       end
     end
   end
