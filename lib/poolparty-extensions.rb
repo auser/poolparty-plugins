5d0732311ea3e9ad8bc62e56773ee00c094bd434
Nate Murray <nate@natemurray.com>
2009-06-23 15:56:05 -0700

    changed loading of extensions. now adds templates to the load paths which makes template loading nice and clean

--- lib/poolparty-extensions.rb
+++ lib/poolparty-extensions.rb
@@ -1,6 +1,9 @@
 class PoolpartyExtensions
 end
 
-Dir["#{File.dirname(__FILE__)}/extensions/*"].each do |lib|
-  require lib
+["#{File.dirname(__FILE__)}/extensions/*.rb", "#{File.dirname(__FILE__)}/extensions/*/*.rb"].each do |dir|
+  Dir[dir].each do |lib|
+    PoolParty::Resources::File.add_searchable_path(File.dirname(lib)) if File.exists?(File.dirname(lib) + "/templates")
+    require lib if ::File.stat(lib).file?
+  end
 end
