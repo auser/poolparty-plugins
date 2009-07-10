56a84e9dc9441c2103396a424638bdd98382d38d
Nate Murray <nate@natemurray.com>
2009-07-10 15:04:51 -0700

    tweaks to shorewall config

--- lib/extensions/shorewall/shorewall.rb
+++ lib/extensions/shorewall/shorewall.rb
@@ -18,7 +18,18 @@ templates/etc/shorewall relative to your clouds.rb. Any files in that directory
 will overide the plugin defaults and be compiled and placed into /etc/shorewall
 on the remote machine. 
 
+== Motivation
+You may ask, "Why do we need a firewall when EC2 already provides one?" The answer is 
+
+* You may be deploying to a cloud provider other than EC2 (VMware, Eucalyptus,
+  Slicehost, homegrown cloud etc.) and they may not have a firewall enabled
+* You may be part of a large organization all sharing one EC2 account. In this
+  case anyone with access to the account could accidentally open ports in the
+  security group.
+* You may need to log various packets for corporate compliance
+
 == References
+* http://www.shorewall.net/standalone.htm
 =end
 
 
@@ -38,9 +49,6 @@ module PoolParty
         end
       end
 
-      # def after_loaded(o={}, &block)
-      # end
-
       def install
         has_package "shorewall-common" 
         has_package "shorewall-perl"
@@ -59,7 +67,6 @@ module PoolParty
       end
 
       def start
-        # has_exec "/sbin/shorewall start"
         has_exec "/sbin/shorewall try /etc/shorewall"
       end
 
