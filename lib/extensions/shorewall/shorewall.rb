f402300cb9d245dba1227626ba1fe4e85f88dabe
Nate Murray <nate@natemurray.com>
2009-07-10 12:08:22 -0700

    tweaks to shorewall rules. now accepts a "rules" directive in your clouds.rb

--- lib/extensions/shorewall/shorewall.rb
+++ lib/extensions/shorewall/shorewall.rb
@@ -6,8 +6,11 @@ Install and configure Shorewall Firewall
 == Usage
 
   shorewall do
-
+    rule "Web/ACCEPT net $FW"
+    rule "SSH/ACCEPT net $FW"
   end
+
+** NOTE! You'd better enable SSH or you'll be locked out of your instance! **
 
 By default this plugin installs the "one-interface" example with some tweaks.
 If you wish to use your own shorewall configs simply create the directory
@@ -23,15 +26,41 @@ module PoolParty
   module Plugin
     class Shorewall < Plugin
 
+      def before_load(o={}, &block)
+        @rules = []
+      end
+
       def loaded(o={}, &block)
         do_once do
           install
+          configure
+          start
         end
       end
+
+      # def after_loaded(o={}, &block)
+      # end
 
       def install
         has_package "shorewall-common" 
         has_package "shorewall-perl"
+        has_directory "/var/lock/subsys"
+      end
+
+      def configure
+        has_variable "shorewall_rules", :value => (@rules.join("\n") || "#")
+        %w{interfaces policy rules zones shorewall.conf}.each do |cfg| # todo, load anything relative to the clouds.rb
+          has_file :name => "/etc/shorewall/#{cfg}", :mode => "0644", :template => "etc/shorewall/#{cfg}.erb"
+        end
+      end
+
+      def rule(rule)
+        @rules << rule
+      end
+
+      def start
+        # has_exec "/sbin/shorewall start"
+        has_exec "/sbin/shorewall try /etc/shorewall"
       end
 
     end
