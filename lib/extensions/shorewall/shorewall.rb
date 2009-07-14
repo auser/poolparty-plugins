559489c4c912551b4313ed9c13280706ade414ff
Nate Murray <nate@natemurray.com>
2009-07-14 09:52:55 -0700

    tweaks to shorewall extensions

--- lib/extensions/shorewall/shorewall.rb
+++ lib/extensions/shorewall/shorewall.rb
@@ -8,15 +8,19 @@ Install and configure Shorewall Firewall
   shorewall do
     rule "Web/ACCEPT net $FW"
     rule "SSH/ACCEPT net $FW"
+    rule "ACCEPT net:10.0.0.0/8 $FW"    # allow local EC2 traffic OR
+    rule "ACCEPT net:192.168.0.0/8 $FW" # allow local class C traffic
   end
 
-** NOTE! You'd better enable SSH or you'll be locked out of your instance! **
+** NOTE! You need to enable SSH or you'll be locked out of your instance! **
 
 By default this plugin installs the "one-interface" example with some tweaks.
 If you wish to use your own shorewall configs simply create the directory
 templates/etc/shorewall relative to your clouds.rb. Any files in that directory
 will overide the plugin defaults and be compiled and placed into /etc/shorewall
 on the remote machine. 
+
+To view the created iptables run: `iptables -L`
 
 == Motivation
 You may ask, "Why do we need a firewall when EC2 already provides one?" The answer is 
@@ -45,7 +49,6 @@ module PoolParty
         do_once do
           install
           configure
-          start
         end
       end
 
@@ -56,18 +59,15 @@ module PoolParty
       end
 
       def configure
+        has_exec :name => "reload_shorewall_config", :command => "/sbin/shorewall try /etc/shorewall", :action => :nothing
         has_variable "shorewall_rules", :value => (@rules.join("\n") || "#")
         %w{interfaces policy rules zones shorewall.conf}.each do |cfg| # todo, load anything relative to the clouds.rb
-          has_file :name => "/etc/shorewall/#{cfg}", :mode => "0644", :template => "etc/shorewall/#{cfg}.erb"
+          has_file :name => "/etc/shorewall/#{cfg}", :mode => "0644", :template => "etc/shorewall/#{cfg}.erb", :calls => get_exec("reload_shorewall_config")
         end
       end
 
       def rule(rule)
         @rules << rule
-      end
-
-      def start
-        has_exec "/sbin/shorewall try /etc/shorewall"
       end
 
     end
